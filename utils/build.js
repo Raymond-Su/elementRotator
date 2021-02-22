e38a8cc41b41b206e8df9263459bfa4d08d36f32
Raymond Su <>
2021-02-22 23:52:09 +1100

    initial commit

---
+++ utils/build.js
@@ -0,0 +1,12 @@
+var webpack = require("webpack"),
+  CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
+  config = require("../webpack.config");
+
+config.plugins = [
+  // clean the build folder
+  new CleanWebpackPlugin(),
+].concat(config.plugins);
+
+webpack(config, function (err) {
+  if (err) throw err;
+});
