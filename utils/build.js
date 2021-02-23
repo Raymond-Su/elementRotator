6c551732b235790dc19d84e6a69c32447dc52d14
Raymond.Su <raymond.su@accenture.com>
2021-02-23 22:23:32 +1100

    Updated build script to clean folder

--- utils/build.js
+++ utils/build.js
@@ -1,8 +1,11 @@
 const webpack = require("webpack"),
+  fs = require('fs-extra'),
   CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
 let config = require("../webpack.config");
 
 console.log("cleaning build folder")
+fs.emptyDirSync('../build')
+
 config.plugins = [
   // clean the build folder
   new CleanWebpackPlugin(),
